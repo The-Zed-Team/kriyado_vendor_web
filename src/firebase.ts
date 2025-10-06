47dd2b63e35a0c3fe0df386f7384a2a15569da00
anirudh-mk <its.me.anirudh.mk@gmail.com>
2025-10-06 21:44:37 +0530

    fix(white screen issue)

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,13 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCdTTXVqW31KOYENyMYyka3hmBaBfvH80Q",
-  authDomain: "vehiclefinder-45de4.firebaseapp.com",
-  projectId: "vehiclefinder-45de4",
-  storageBucket: "vehiclefinder-45de4.firebasestorage.app",
-  messagingSenderId: "632261686251",
-  appId: "1:632261686251:web:3930ca9dffd4214fc19fae"
-  // Add other config values as needed
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 initializeApp(firebaseConfig);
